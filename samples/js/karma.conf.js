9939846db5e50bc0169833781501205d77d96d92
Mark Adams <mark@markadams.me>
2017-03-31 22:26:26 -0500

    Update samples/js so it works again
    samples/js was broken after I switched it to Node 7.x because EventEmitter.prototype was removed.
    
    Due to some dependency weirdness, I actually had to add an npm-shrinkwrap.json file to ensure that we would load a new enough version of socket.io that was compatible with 7.x.
    
    While I was in here, I also removed the junit reporter (no idea why I had it here in the first place).
    
    Fixes #23

--- samples/js/karma.conf.js
+++ samples/js/karma.conf.js
@@ -2,13 +2,11 @@ module.exports = function(config) {
   config.set({
     browsers: ['Chrome'],
     frameworks: ['qunit'],
+    plugins: ['karma-qunit', 'karma-chrome-launcher'],
     files: [
       'tests/*.js'
     ],
     singleRun: true,
-    reporters: ['progress', 'junit'],
-    junitReporter: {
-      outputFile: 'test-results/results.js.xml'
-    }
+    reporters: ['progress']
   });
 };
